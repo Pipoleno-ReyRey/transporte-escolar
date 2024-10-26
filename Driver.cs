f827bf8f8d5c4688961531a9b729a659c79dd1e2
Reynaldo Suero Delmonte <145385282+Pipoleno-ReyRey@users.noreply.github.com>
2024-10-26 19:43:46 -0400

    Delete Driver.cs

--- Driver.cs
+++
@@ -1,8 +0,0 @@
-public class Driver{
-    public int id {set; get;}
-    public string? name {set; get;}
-    public float salary {set; get;}
-    public string? license {set; get;}
-    public int vehicleId {set; get;}
-
-}
