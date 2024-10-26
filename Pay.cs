67542aab0d133986b1d989e9a59e408fd56379f6
Reynaldo Suero Delmonte <145385282+Pipoleno-ReyRey@users.noreply.github.com>
2024-10-26 19:45:03 -0400

    Delete Pay.cs

--- Pay.cs
+++
@@ -1,8 +0,0 @@
-public class Pays
-{
-    public int id {set; get;}
-    public int studentId {set; get;}
-    public float count {set; get;}
-    public DateTime datePay {set; get;}
-    public Boolean state {set; get;}
-}
